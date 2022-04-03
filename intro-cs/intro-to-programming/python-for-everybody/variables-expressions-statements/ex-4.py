6f0bda923948dd5cff005ad9c3856f3d46ab323e
dmytro.sheiko <c-dmytro.sheiko@hulu.com>
2022-04-03 13:21:03 +0300

    finish: variables, statesments and expressions

---
+++ intro-cs/intro-to-programming/python-for-everybody/variables-expressions-statements/ex-4.py
@@ -0,0 +1,12 @@
+width = 17
+height = 12.0
+
+first = width // 2
+second = width / 2.0
+third = height / 3
+fourth = 1 + 2 * 5
+
+print("First: value - " + str(first) + ", type - " + str(type(first)))
+print("Second: value - " + str(second) + ", type - " + str(type(second)))
+print("Third: value - " + str(third) + ", type - " + str(type(third)))
+print("Fourth: value - " + str(fourth) + ", type - " + str(type(fourth)))
---
+++ intro-cs/intro-to-programming/python-for-everybody/variables-expressions-statements/ex-5.py
@@ -0,0 +1,4 @@
+temperatureInCelsius = int(input("Enter temperature in Celsius: ")) 
+temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32
+
+print("Temperature in Fahrenheit: " + str(temperatureInFahrenheit))
